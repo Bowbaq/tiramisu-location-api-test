34730e5deb8e412f3ab09b4f168970c797b4c00c
Maxime Bury <maxime.bury@gmail.com>
2013-02-06 10:00:32 -0500

    Make dates more readable

--- www/js/watch.js
+++ www/js/watch.js
@@ -1,9 +1,10 @@
 var socket = io.connect("http://sleepy-brook-7383.herokuapp.com");
+var start = moment();
 
 socket.emit('join:display');
 socket.on('location', function(location) {
   var data = '<tr><td>{{TIME}}</td><td>{{LAT}}</td><td>{{LNG}}</td></tr>'
-    .replace('{{TIME}}', location.time)
+    .replace('{{TIME}}', moment(location.time).from(start))
     .replace('{{LAT}}', location.lat)
     .replace('{{LNG}}', location.lng)
   ;
